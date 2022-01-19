2102e81c700aa624a71d470034c0fc5eac935fa5
shinhong_park <shp7724@snu.ac.kr>
2022-01-19 16:08:47 +0900

    configmap 파일 생성

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wzeo3zd6$mev!&x7j#5w$jc($^di#m#+i68182m=#8rfi^eefs'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-wzeo3zd6$mev!&x7j#5w$jc($^di#m#+i68182m=#8rfi^eefs')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = os.getenv('DEBUG', True)
+ALLOWED_HOSTS = [os.getenv('DJANGO_ALLOWED_HOST', '*')]
 
 # Application definition
 
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd k8s
 kubectl apply -f deployment.yaml
+kubectl apply -f configmap.yaml
 kubectl rollout restart deployment/backend-deployment
