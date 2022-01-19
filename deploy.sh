c14d79076e5a97fbb80541134579af819cffd503
shinhong_park <shp7724@naver.com>
2022-01-19 16:28:10 +0900

    deploy 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd k8s
-kubectl apply -f deployment.yaml
-kubectl apply -f configmap.yaml
-kubectl rollout restart deployment/backend-deployment
+microk8s kubectl apply -f deployment.yaml
+microk8s kubectl apply -f configmap.yaml
+microk8s kubectl rollout restart deployment/backend-deployment
