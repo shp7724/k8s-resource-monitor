e7292e01c4597b95699d5da87f5d1332ddf5ea18
shinhong_park <shp7724@snu.ac.kr>
2022-01-24 13:11:33 +0900

    namespace 추가, 파일구조 리팩토링

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd k8s
-microk8s kubectl apply -f deployment.yaml
-microk8s kubectl apply -f configmap.yaml
-microk8s kubectl rollout restart deployment/backend-deployment
+kubectl apply -f namespace.yaml
+kubectl apply -f configmap.yaml
+kubectl apply -f deployment.yaml
+kubectl rollout restart deployment/django-k8s-resource-monitor-deployment
