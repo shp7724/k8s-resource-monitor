3dd20949890d0b47bfff633b41c67ca24c6918a7
shinhong_park <shp7724@snu.ac.kr>
2022-01-26 11:16:59 +0900

    프론트엔드 배포 완료

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 cd backend
 docker build . -t shp7724/django-k8s-resource-monitor:latest
 docker push shp7724/django-k8s-resource-monitor:latest
+cd ../frontend
+docker build . -t shp7724/react-k8s-resource-monitor:latest
+docker push shp7724/react-k8s-resource-monitor:latest
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 cd devops
 kubectl apply -f namespace.yaml
 kubectl apply -f configmap.yaml
-kubectl apply -f deployment.yaml
+kubectl apply -f django-deployment.yaml
+kubectl apply -f react-deployment.yaml
 kubectl rollout restart deployment/django-k8s-resource-monitor-deployment -n backend-system
+kubectl rollout restart deployment/react-k8s-resource-monitor-deployment -n frontend-system
