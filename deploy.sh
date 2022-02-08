238d5b4cab17e85c519c22f4e2c404a1ef55d94a
shinhong_park <shp7724@snu.ac.kr>
2022-02-08 11:44:10 +0900

    namespace 변경

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ kubectl apply -f namespace.yaml
 kubectl apply -f configmap.yaml
 kubectl apply -f django-deployment.yaml
 kubectl apply -f react-deployment.yaml
-kubectl rollout restart deployment/django-k8s-resource-monitor-deployment -n frontend-system
-kubectl rollout restart deployment/react-k8s-resource-monitor-deployment -n frontend-system
+kubectl rollout restart deployment/django-k8s-resource-monitor-deployment -n resource-monitor-system
+kubectl rollout restart deployment/react-k8s-resource-monitor-deployment -n resource-monitor-system
