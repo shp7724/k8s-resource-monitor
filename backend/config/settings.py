0014f4b21dc24e8f44603e398b228952d5f5d223
shinhong_park <shp7724@snu.ac.kr>
2022-02-14 02:02:16 +0900

    환경 변수 세팅 코드 추가

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -101,9 +101,6 @@ DATABASES = {
     }
 }
 
-with open('debug.log','w') as f:
-    f.write(os.getenv("DJANGO_DB_PASSWORD", "fake-password"))
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
