5b3f061e355b4001b92a83dc7c3a814c575a227a
shinhong_park <shp7724@snu.ac.kr>
2022-02-14 01:42:19 +0900

    터미널 인증 프론트 대응

--- frontend/src/components/pods/Terminal.tsx
+++ frontend/src/components/pods/Terminal.tsx
@@ -5,19 +5,29 @@ import { FitAddon } from "xterm-addon-fit";
 import { XTerm } from "xterm-for-react";
 import shallow from "zustand/shallow";
 import { endpoint, isProduction } from "../../common/axios";
+import { useAuth } from "../../states/auth";
 import { useTerminal } from "../../states/pods";
 
 const Terminal: FC = (): JSX.Element => {
   const xtermRef = useRef<any>(null);
   const fitAddon = new FitAddon();
+  const { isAuthenticated, token } = useAuth(
+    (state) => ({
+      isAuthenticated: state.isAuthenticated,
+      token: state.accessToken,
+    }),
+    shallow
+  );
   const { isOpen, containerName, podName, namespace } = useTerminal(
     (state) => state,
     shallow
   );
+
   const { sendMessage, lastMessage, readyState } = useWebSocket(
     `${
       isProduction ? "wss" : "ws"
-    }://${endpoint}/ws/ssh/pod/${namespace}/${podName}/${containerName}/`
+    }://${endpoint}/ws/ssh/pod/${namespace}/${podName}/${containerName}/`,
+    { protocols: ["Bearer", token || "fake-token"] }
   );
 
   useEffect(() => {
@@ -31,6 +41,12 @@ const Terminal: FC = (): JSX.Element => {
         xtermRef.current.terminal.writeln(`Closing connection...`);
         break;
       case ReadyState.CLOSED:
+        if (!isAuthenticated) {
+          xtermRef.current.terminal.writeln(
+            `Permission denied. Please sign in first.`
+          );
+          break;
+        }
         xtermRef.current.terminal.writeln(
           `An error occurred while establishing connection.`
         );
